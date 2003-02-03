aef1088719e45ed8abadd5987bc0da37423ffe11
craig <>
2003-02-03 16:18:50 +0000

    initial import into CVS

---
+++ bin/martexplorer.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Note: /bin/sh doesn't work on Alphas (need to use bash thexre) but
+# works everywhere else.
+
+# Starts the MartExplorer application.
+
+TMP_ROOT=`dirname $0`/..
+
+TMP_CLASSPATH=${TMP_ROOT}/build/classes 
+TMP_CLASSPATH=${TMP_CLASSPATH}:${TMP_ROOT}/lib/mart-explorer.jar
+TMP_CLASSPATH=${TMP_CLASSPATH}:${TMP_ROOT}/lib/mysql-connector-java-2.0.14-bin.jar
+TMP_CLASSPATH=${TMP_CLASSPATH}:${TMP_ROOT}/lib/log4j-1.2.6.jar
+
+java -classpath ${TMP_CLASSPATH} $@ org.ensembl.mart.explorer.MartExplorerApplication
