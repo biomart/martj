28d83a0e18cfa4ef111c250e1a18b61c6024efb6
craig <>
2003-02-04 19:22:27 +0000

    Added some partially implemented classes and interfaces.
    Application mode selection supported (command line or gui). Initial work on command line mode done, makes connection to db.
    
    Added log4j support.

--- bin/martexplorer.sh
+++ bin/martexplorer.sh
@@ -5,6 +5,10 @@
 
 # Starts the MartExplorer application.
 
+# Usage:
+#
+# prompt> bin/martexplorer.sh
+
 TMP_ROOT=`dirname $0`/..
 
 TMP_CLASSPATH=${TMP_ROOT}/build/classes 
@@ -12,4 +16,6 @@ TMP_CLASSPATH=${TMP_CLASSPATH}:${TMP_ROOT}/lib/mart-explorer.jar
 TMP_CLASSPATH=${TMP_CLASSPATH}:${TMP_ROOT}/lib/mysql-connector-java-2.0.14-bin.jar
 TMP_CLASSPATH=${TMP_CLASSPATH}:${TMP_ROOT}/lib/log4j-1.2.6.jar
 
-java -classpath ${TMP_CLASSPATH} $@ org.ensembl.mart.explorer.MartExplorerApplication
+java -classpath ${TMP_CLASSPATH} org.ensembl.mart.explorer.MartExplorerApplication $@
+
+
