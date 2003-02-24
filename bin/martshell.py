fa9b403e5bec4614e8d391db443a1d8d4d46accf
craig <>
2003-02-24 18:12:13 +0000

    Added jython interactive shell for mart explorer.

---
+++ bin/martshell.py
@@ -0,0 +1,10 @@
+from org.ensembl.mart.explorer import *
+from org.ensembl.mart.explorer.gui import *
+
+MartExplorerApplication.defaultLoggingConfiguration()
+
+print "*************"
+print "* MartShell *"
+print "*************"
+
+
