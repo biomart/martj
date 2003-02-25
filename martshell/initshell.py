995490334989960e484cc037767b319520e09ce4
craig <>
2003-02-25 11:51:21 +0000

    Added trick for setting up python path without having to modify user's environment environment.

--- martshell/initshell.py
+++ martshell/initshell.py
@@ -2,6 +2,11 @@
 
 from org.ensembl.mart.explorer import *
 from org.ensembl.mart.explorer.gui import *
+import sys
+
+# add the parent directory of this directory to python's path so that the
+# user does't have to set it
+sys.path.append( sys.path[0]+"/..")
 
 MartExplorerApplication.defaultLoggingConfiguration()
 
