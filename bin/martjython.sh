8f36d950ea0c425cc2508db2064bf14bdf27cb03
craig <>
2003-04-03 13:33:33 +0000

    Script for running jython programs that doesn't require any environment settings.

---
+++ bin/martjython.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# Note: /bin/sh doesn't work on Alphas (need to use bash thexre) but
+# works everywhere else.
+
+# Runs the specified jython script.
+
+# Usage:
+#
+# prompt> bin/martjython.sh PYTHON_PROGRAM.py
+
+# unless python.cachedir is set in ~/.jython a cachedir will be
+# automatically created. The cache speeds up the shell. It can be
+# safely deleted but will be automatically recreated next time you run
+# this program.
+
+CACHE_DIR=${HOME}/.martshell_cachedir
+
+TMP_ROOT=`dirname $0`/..
+
+TMP_CLASSPATH=${TMP_ROOT}/build/classes 
+TMP_CLASSPATH=${TMP_CLASSPATH}:${TMP_ROOT}/lib/mart-explorer.jar
+TMP_CLASSPATH=${TMP_CLASSPATH}:${TMP_ROOT}/lib/mysql-connector-java-2.0.14-bin.jar
+TMP_CLASSPATH=${TMP_CLASSPATH}:${TMP_ROOT}/lib/log4j-1.2.6.jar
+TMP_CLASSPATH=${TMP_CLASSPATH}:${TMP_ROOT}/lib/jython.jar
+TMP_CLASSPATH=${TMP_CLASSPATH}:${TMP_ROOT}/lib/junit.jar
+
+CMD="java -classpath ${TMP_CLASSPATH} -Dpython.cachedir=${CACHE_DIR} org.python.util.jython $1"
+
+#echo $CMD
+$CMD
