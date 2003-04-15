67166e04f1b2f45d96ce4d5af029c28d6be557d8
dlondon <>
2003-04-15 06:53:11 +0000

    *** empty log message ***

--- src/java/org/ensembl/mart/explorer/MartExplorerTool.java
+++ src/java/org/ensembl/mart/explorer/MartExplorerTool.java
@@ -247,7 +247,7 @@ public class MartExplorerTool {
     else {
 	    // all required attributes set, may need to set some defaults
 		if (formatspec.getFormat() == FormatSpec.TABULATED && formatspec.getSeparator() == null) {
-            logger.info("No separator specified for tabulated output, defaulting to tab separated");
+            logger.warn("No separator specified for tabulated output, defaulting to tab separated");
             formatspec.setSeparator("\t");
 		}
 	}
