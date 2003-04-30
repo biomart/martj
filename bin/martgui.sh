ad06a3f2cacefc87c4f22bbb18bde66c49615704
craig <>
2003-04-30 15:12:12 +0000

    *** empty log message ***

--- bin/martgui.sh
+++ bin/martgui.sh
@@ -3,19 +3,11 @@
 # Note: /bin/sh doesn't work on Alphas (need to use bash thexre) but
 # works everywhere else.
 
-# Starts the MartExplorer application.
+# Starts the MartExplorer GUI application.
 
 # Usage:
 #
-# prompt> bin/martexplorer.sh
+# prompt> bin/martgui.sh
 
 TMP_ROOT=`dirname $0`/..
-
-TMP_CLASSPATH=${TMP_ROOT}/build/classes
-TMP_CLASSPATH=${TMP_CLASSPATH}:${TMP_ROOT}/lib/mart-explorer.jar
-TMP_CLASSPATH=${TMP_CLASSPATH}:${TMP_ROOT}/lib/mysql-connector-java-2.0.14-bin.jar
-TMP_CLASSPATH=${TMP_CLASSPATH}:${TMP_ROOT}/lib/log4j-1.2.6.jar
-TMP_CLASSPATH=${TMP_CLASSPATH}:${TMP_ROOT}/lib/java-getopt-1.0.9.jar
-TMP_CLASSPATH=${TMP_CLASSPATH}:${TMP_ROOT}/lib/jython.jar
-
-java -classpath ${TMP_CLASSPATH} org.ensembl.mart.explorer.gui.MartExplorerApplication $@
+java -jar ${TMP_ROOT}/lib/mart-explorer.jar $@
