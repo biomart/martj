bd49fcffdd9ca3a3530425167c974d9a9f9ed2e8
dlondon <>
2003-05-08 16:43:48 +0000

    tests for presence of src MartExplorerGUIApplication.py script.  If present, it uses it, else it uses the mart-explorer.jar

--- bin/martgui.sh
+++ bin/martgui.sh
@@ -9,5 +9,26 @@
 #
 # prompt> bin/martgui.sh
 
-TMP_ROOT=`dirname $0`/..
-java -jar ${TMP_ROOT}/lib/mart-explorer.jar $@
+if [ -s src/jython/martexplorer/MartExplorerGUIApplication.py ] 
+then
+  CACHE_DIR=${HOME}/.martshell_cachedir
+ 
+  TMP_ROOT=`dirname $0`/..
+ 
+  TMP_CLASSPATH=${TMP_ROOT}/build/classes
+  TMP_CLASSPATH=${TMP_CLASSPATH}:${TMP_ROOT}/lib/mart-explorer.jar
+  TMP_CLASSPATH=${TMP_CLASSPATH}:${TMP_ROOT}/lib/mysql-connector-java-2.0.14-bin.jar
+  TMP_CLASSPATH=${TMP_CLASSPATH}:${TMP_ROOT}/lib/log4j-1.2.6.jar
+  TMP_CLASSPATH=${TMP_CLASSPATH}:${TMP_ROOT}/lib/jython.jar
+  TMP_CLASSPATH=${TMP_CLASSPATH}:${TMP_ROOT}/lib/junit.jar
+ 
+  TMP_JYTHON_LIB=${TMP_ROOT}/lib
+ 
+  CMD="java -classpath ${TMP_CLASSPATH} -Dpython.cachedir=${CACHE_DIR} -Dpython.path=${TMP_JYTHON_LIB} org.python.util.jython ${TMP_ROOT}/src/jython/martexplorer/MartExplorerGUIApplication.py"
+ 
+  #echo $CMD
+  $CMD
+else 
+  TMP_ROOT=`dirname $0`/..
+  java -jar ${TMP_ROOT}/lib/mart-explorer.jar $@
+fi
