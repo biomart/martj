0bcf17b049d49224233d7cb110063a3018290477
craig <>
2003-05-21 14:28:22 +0000

    Updated to use new mysql-driver.

--- bin/martgui.sh
+++ bin/martgui.sh
@@ -18,7 +18,7 @@ then
  
   TMP_CLASSPATH=${TMP_ROOT}/build/classes
   TMP_CLASSPATH=${TMP_CLASSPATH}:${TMP_ROOT}/lib/mart-explorer.jar
-  TMP_CLASSPATH=${TMP_CLASSPATH}:${TMP_ROOT}/lib/mysql-connector-java-2.0.14-bin.jar
+  TMP_CLASSPATH=${TMP_CLASSPATH}:${TMP_ROOT}/lib/mysql-connector-java-3.0.7-stable-bin.jar
   TMP_CLASSPATH=${TMP_CLASSPATH}:${TMP_ROOT}/lib/log4j-1.2.6.jar
   TMP_CLASSPATH=${TMP_CLASSPATH}:${TMP_ROOT}/lib/jython.jar
   TMP_CLASSPATH=${TMP_CLASSPATH}:${TMP_ROOT}/lib/junit.jar
