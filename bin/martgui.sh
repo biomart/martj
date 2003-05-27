fddd98f6f100da7696d59d0fd1b4fb097d99f960
craig <>
2003-05-27 14:40:14 +0000

    bug fixes:
    - command line parameters now passed to script in "development" mode.
    
    - removed mart-explorer.jar from classpath used when run in
    "development" mode.

--- bin/martgui.sh
+++ bin/martgui.sh
@@ -17,7 +17,6 @@ then
   TMP_ROOT=`dirname $0`/..
  
   TMP_CLASSPATH=${TMP_ROOT}/build/classes
-  TMP_CLASSPATH=${TMP_CLASSPATH}:${TMP_ROOT}/lib/mart-explorer.jar
   TMP_CLASSPATH=${TMP_CLASSPATH}:${TMP_ROOT}/lib/mysql-connector-java-3.0.7-stable-bin.jar
   TMP_CLASSPATH=${TMP_CLASSPATH}:${TMP_ROOT}/lib/log4j-1.2.6.jar
   TMP_CLASSPATH=${TMP_CLASSPATH}:${TMP_ROOT}/lib/jython.jar
@@ -25,7 +24,7 @@ then
  
   TMP_JYTHON_LIB=${TMP_ROOT}/lib
  
-  CMD="java -classpath ${TMP_CLASSPATH} -Dpython.cachedir=${CACHE_DIR} -Dpython.path=${TMP_JYTHON_LIB} org.python.util.jython ${TMP_ROOT}/src/jython/martexplorer/MartExplorerGUIApplication.py"
+  CMD="java -classpath ${TMP_CLASSPATH} -Dpython.cachedir=${CACHE_DIR} -Dpython.path=${TMP_JYTHON_LIB} org.python.util.jython ${TMP_ROOT}/src/jython/martexplorer/MartExplorerGUIApplication.py $@"
  
   #echo $CMD
   $CMD
