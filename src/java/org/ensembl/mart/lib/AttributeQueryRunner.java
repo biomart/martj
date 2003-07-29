f33a759ec7d8ba927283023d115788f74b766b23
dlondon <>
2003-07-29 15:37:03 +0000

    implimented batching system to make sure that really large resultsets do not crash the JVM memory.

--- src/java/org/ensembl/mart/lib/AttributeQueryRunner.java
+++ src/java/org/ensembl/mart/lib/AttributeQueryRunner.java
@@ -23,68 +23,91 @@ import org.apache.log4j.Logger;
  */
 public final class AttributeQueryRunner implements QueryRunner {
 
-    /**
-     * Constructs a TabulatedQueryRunner object to execute a Query
-     * and print tabulated output specified by the given FormatSpec
-     * 
-     * @param query - a Query Object
-     * @param format - a FormatSpec object
-     */
-    public AttributeQueryRunner(Query query, FormatSpec format, Connection conn, OutputStream os) {
-        this.query = query;
-        this.format = format;
-        this.conn = conn;
-        this.osr = new OutputStreamWriter(os);
+	/**
+	 * Constructs a TabulatedQueryRunner object to execute a Query
+	 * and print tabulated output specified by the given FormatSpec
+	 * 
+	 * @param query - a Query Object
+	 * @param format - a FormatSpec object
+	 */
+	public AttributeQueryRunner(Query query, FormatSpec format, Connection conn, OutputStream os) {
+		this.query = query;
+		this.format = format;
+		this.conn = conn;
+		this.osr = new OutputStreamWriter(os);
 	}
 
-    public void execute(int limit) throws SequenceException, InvalidQueryException {
-  			try {
-           CompiledSQLQuery csql = new CompiledSQLQuery( conn, query );
-            String sql = csql.toSQL();
-            if (limit > 0)
-              sql = sql+" limit "+limit;
+	public void execute(int limit) throws SequenceException, InvalidQueryException {
+		boolean moreRows = true;
+		// batching output system stops when this is false
+		int batchStart = 0; // start at 0 during batching
 
-            logger.debug( "QUERY : " + query );
-            logger.debug( "SQL : " +sql );
+		String sql = null;
+		try {
+			CompiledSQLQuery csql = new CompiledSQLQuery(conn, query);
+			String sqlbase = csql.toSQL();
 
-            PreparedStatement ps = conn.prepareStatement( sql );
-            int p=1;
-            for( int i=0, n=query.getFilters().length; i<n; ++i) {
-                Filter f = query.getFilters()[i];
-                String value = f.getValue();
-                if ( value!=null ) {
-                  if ( logger.isDebugEnabled()  )
-                    logger.debug("SQL (prepared statement value) : "+p+" = " + value);
-                  ps.setString( p++, value);
-                }
-            }
-            
-            ResultSet rs = ps.executeQuery();
-			
-            while ( rs.next() ) {
-              for (int i = 1, nColumns = rs.getMetaData().getColumnCount(); i <= nColumns; ++i) {
-                if (i>1) osr.write( format.getSeparator() );
-                String v = rs.getString(i);
-                if (  v!= null ) osr.write( v );
-                logger.debug( v );
-              }
-              osr.write("\n");
-              osr.flush();
-            }
-        }
-        catch (IOException e) {
-            logger.warn("Couldnt write to OutputStream\n"+e.getMessage());
-            throw new InvalidQueryException(e);
-		} 
-        catch (SQLException e) {
-            logger.warn(e.getMessage());
-            throw new InvalidQueryException(e);
-        }
+			while (moreRows) {
+        sql = sqlbase;
+        
+				if (limit > 0) {
+					sql = sql + " limit " + limit;
+					moreRows = false;
+					// for client determined limits, assume they are sane, and run the entire query
+				} else {
+					sql += " limit " + batchStart + ", " + batchLength;
+					batchStart += batchLength;
+				}
+
+				logger.debug("QUERY : " + query);
+				logger.debug("SQL : " + sql);
+
+				PreparedStatement ps = conn.prepareStatement(sql);
+				int p = 1;
+				for (int i = 0, n = query.getFilters().length; i < n; ++i) {
+					Filter f = query.getFilters()[i];
+					String value = f.getValue();
+					if (value != null) {
+						if (logger.isDebugEnabled())
+							logger.debug("SQL (prepared statement value) : " + p + " = " + value);
+						ps.setString(p++, value);
+					}
+				}
+
+				ResultSet rs = ps.executeQuery();
+
+        int rows = 0;
+				while (rs.next()) {
+					rows++;
+					
+					for (int i = 1, nColumns = rs.getMetaData().getColumnCount(); i <= nColumns; ++i) {
+						if (i > 1)
+							osr.write(format.getSeparator());
+						String v = rs.getString(i);
+						if (v != null)
+							osr.write(v);
+						logger.debug(v);
+					}
+					osr.write("\n");
+					osr.flush();
+				}
+				if (rows < batchLength)
+					moreRows = false;
+				// on the odd chance that the last result set is equal in size to the batchLength, it will need to make an extra attempt.
+			}
+		} catch (IOException e) {
+			logger.warn("Couldnt write to OutputStream\n" + e.getMessage());
+			throw new InvalidQueryException(e);
+		} catch (SQLException e) {
+			logger.warn(e.getMessage());
+			throw new InvalidQueryException(e);
+		}
 	}
 
-    private Logger logger = Logger.getLogger(AttributeQueryRunner.class.getName());
-    private Query query = null;
-    private FormatSpec format = null;
-    private OutputStreamWriter osr;
-    private Connection conn;
+	private int batchLength = 200000;
+	private Logger logger = Logger.getLogger(AttributeQueryRunner.class.getName());
+	private Query query = null;
+	private FormatSpec format = null;
+	private OutputStreamWriter osr;
+	private Connection conn;
 }
