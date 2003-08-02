5a785609a6d8e1fa0b76964e7fb2063d6bc325b5
craig <>
2003-08-02 09:27:03 +0000

    reordered elemts in file.

--- src/java/org/ensembl/mart/explorer/MartExplorer.java
+++ src/java/org/ensembl/mart/explorer/MartExplorer.java
@@ -47,13 +47,29 @@ import org.ensembl.mart.lib.config.MartConfigurationFactory;
  * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
  */
 public class MartExplorer extends JPanel {
-	
-	private static final Dimension PREFERRED_SIZE = new Dimension( 700, 700 );
 	private static final String CONFIG_FILE_KEY = "CONFIG_FILE_KEY";
 	
-  private Preferences prefs;
+	private static final Dimension PREFERRED_SIZE = new Dimension( 1024, 768 );
+
+	
+
+
+	
+
+
+	public static void main(String[] args) {
+		MartExplorer me = new MartExplorer();
+		JFrame f = new JFrame( "Mart Explorer" );
+		f.getContentPane().add( me );
+		f.setJMenuBar( me.createMenuBar() );
+		f.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+		f.pack();
+		f.setVisible( true );
+	}
  	private JFileChooser configFileChooser;
 	private DatabaseSettingsDialog databaseSettings;
+	
+  private Preferences prefs;
   
     
 	private QueryManager queryManager = new QueryManager();
@@ -70,131 +86,18 @@ public class MartExplorer extends JPanel {
 		setMaximumSize( PREFERRED_SIZE );	
 
 	}
-
-
-	/**
-	 * Initialises _configFileChooser_. Sets the last loaded 
-   * config file if available and makes the 
-   * chooser only show XML files.
-	 */
-	private void initConfigFileChooser() {
-    configFileChooser = new JFileChooser();
-		FileFilter xmlFilter = new FileFilter() {
-			public boolean accept(File f) {
-				return f != null
-					&& (f.isDirectory()
-						|| f.getName().toLowerCase().endsWith(".xml"));
-			}
-			public String getDescription() {
-				return "XML Files";
-			}
-		};
-
-		String lastChosenFile = prefs.get(CONFIG_FILE_KEY, null);
-
-		if (lastChosenFile != null) {
-			configFileChooser.setSelectedFile(new File(lastChosenFile));
-		}
-		configFileChooser.addChoosableFileFilter(xmlFilter);
-
-	}
-
-
-	private void initDatabaseSettings() {
-    databaseSettings = new DatabaseSettingsDialog();
-    
-    // load supported database types into preferences. We do this rather than just 
-    // loading them directly because if there are more than one wee don't want to 
-    // override the last selected one.
-    String key = databaseSettings.getDatabaseType().getPreferenceKey();
-    String current = prefs.get(key, null);
-    if ( current==null || current.length()==0 ) {
-      prefs.put(key, "mysql" );   
-    }
-		databaseSettings.setPrefs( prefs );
-	}
-
-
-	public void warn(String message) {
-		JOptionPane.showMessageDialog(
-			this,
-			message,
-			"Warning",
-			JOptionPane.WARNING_MESSAGE);
-	}
-
-	public void warn(String message, Exception e) {
-		warn(message + ":" + e.getMessage());
-		e.printStackTrace();
-	}
-
-	public void connectToDatabase() {
-		if ( databaseSettings.showDialog(this) ) {
-			System.out.println("todo - use db settings");
-		}
-	}
-
-	/**
-	 * Presents the user with a file chooser dialog with which she can 
-	 * choose a configuration file. 
-	 */
-	public void loadConfigFromFile() {
-		// user chooses file
-		
-		int action = configFileChooser.showOpenDialog(this);
-
-		// convert file contents into string
-		if (action == JFileChooser.APPROVE_OPTION) {
-			File f = configFileChooser.getSelectedFile().getAbsoluteFile();
-			prefs.put( CONFIG_FILE_KEY, f.toString() ) ;
-			try {
-				MartConfiguration config = new MartConfigurationFactory().getInstance( f.toURL() );
-			} catch (MalformedURLException e) {
-				warn("Couldn't find file. " + e);
-			} catch (ConfigurationException e) {
-				warn( "Problem loading configuration file. ", e );
-			}
-							
-		}
-	}
-
-
-
-	/**
-	 * Exits the programs.
-	 */
-	public void exit() {
-		System.exit(0);	
-	}
-
-	/**
-		 * 
-		 */
-		public void newQuery() {
-			// TODO Auto-generated method stub
-		
-		}
-		
-
-	public void executeQuery() {
-				// TODO Auto-generated method stub
-		
-			}
-			
-	public void save() {
-				// TODO Auto-generated method stub
-		
-			}
-
-	public void saveAs() {
-				// TODO Auto-generated method stub
-		
-			}
 			
 	public void about() {
 				// TODO Auto-generated method stub
 		
 			}
+
+	public void connectToDatabase() {
+		if (databaseSettings.showDialog(this)) {
+              
+			System.out.println("todo - use db settings");
+		}
+	}
 
 	/**
 	 * @return
@@ -280,20 +183,118 @@ public class MartExplorer extends JPanel {
 		all.add( help );
 		return all;
 	}
+		
 
-	
+	public void executeQuery() {
+				// TODO Auto-generated method stub
+		
+			}
 
 
-	
+
+	/**
+	 * Exits the programs.
+	 */
+	public void exit() {
+		System.exit(0);	
+	}
 
 
-	public static void main(String[] args) {
-		MartExplorer me = new MartExplorer();
-		JFrame f = new JFrame( "Mart Explorer" );
-		f.getContentPane().add( me );
-		f.setJMenuBar( me.createMenuBar() );
-		f.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-		f.pack();
-		f.setVisible( true );
+	/**
+	 * Initialises _configFileChooser_. Sets the last loaded 
+   * config file if available and makes the 
+   * chooser only show XML files.
+	 */
+	private void initConfigFileChooser() {
+    configFileChooser = new JFileChooser();
+		FileFilter xmlFilter = new FileFilter() {
+			public boolean accept(File f) {
+				return f != null
+					&& (f.isDirectory()
+						|| f.getName().toLowerCase().endsWith(".xml"));
+			}
+			public String getDescription() {
+				return "XML Files";
+			}
+		};
+
+		String lastChosenFile = prefs.get(CONFIG_FILE_KEY, null);
+
+		if (lastChosenFile != null) {
+			configFileChooser.setSelectedFile(new File(lastChosenFile));
+		}
+		configFileChooser.addChoosableFileFilter(xmlFilter);
+
+	}
+
+
+	private void initDatabaseSettings() {
+    databaseSettings = new DatabaseSettingsDialog();
+    
+    // load supported database types into preferences. We do this rather than just 
+    // loading them directly because if there are more than one wee don't want to 
+    // override the last selected one.
+    String key = databaseSettings.getDatabaseType().getPreferenceKey();
+    String current = prefs.get(key, null);
+    if ( current==null || current.length()==0 ) {
+      prefs.put(key, "mysql" );   
+    }
+		databaseSettings.setPrefs( prefs );
+	}
+
+	/**
+	 * Presents the user with a file chooser dialog with which she can 
+	 * choose a configuration file. 
+	 */
+	public void loadConfigFromFile() {
+		// user chooses file
+		
+		int action = configFileChooser.showOpenDialog(this);
+
+		// convert file contents into string
+		if (action == JFileChooser.APPROVE_OPTION) {
+			File f = configFileChooser.getSelectedFile().getAbsoluteFile();
+			prefs.put( CONFIG_FILE_KEY, f.toString() ) ;
+			try {
+				MartConfiguration config = new MartConfigurationFactory().getInstance( f.toURL() );
+			} catch (MalformedURLException e) {
+				warn("Couldn't find file. " + e);
+			} catch (ConfigurationException e) {
+				warn( "Problem loading configuration file. ", e );
+			}
+							
+		}
+	}
+
+	/**
+		 * 
+		 */
+		public void newQuery() {
+			// TODO Auto-generated method stub
+		
+		}
+			
+	public void save() {
+				// TODO Auto-generated method stub
+		
+			}
+
+	public void saveAs() {
+				// TODO Auto-generated method stub
+		
+			}
+
+
+	public void warn(String message) {
+		JOptionPane.showMessageDialog(
+			this,
+			message,
+			"Warning",
+			JOptionPane.WARNING_MESSAGE);
+	}
+
+	public void warn(String message, Exception e) {
+		warn(message + ":" + e.getMessage());
+		e.printStackTrace();
 	}
 }
