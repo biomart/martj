ef719b47b71a0da9041bb4e358e17337c9480a5d
craig <>
2003-08-14 12:11:26 +0000

    Bug fix: changing the dataset now causes the tree and inputPanel to be updated correctly.

--- src/java/org/ensembl/mart/explorer/QueryEditor.java
+++ src/java/org/ensembl/mart/explorer/QueryEditor.java
@@ -54,241 +54,259 @@ import org.ensembl.mart.lib.config.MartConfigurationFactory;
  * MartConfiguration object it is initialised with.
  * </p>
  */
-public class QueryEditor extends JPanel implements PropertyChangeListener, TreeSelectionListener  {
+public class QueryEditor
+	extends JPanel
+	implements PropertyChangeListener, TreeSelectionListener {
 
-  private static final Logger logger = Logger.getLogger( QueryEditor.class.getName() );
+	private static final Logger logger =
+		Logger.getLogger(QueryEditor.class.getName());
 
-  /** total height of the componment */
-  private int HEIGHT = 768;
-  
-  /** total width of the component */
-  private int WIDTH = 1024;
-  
-  /** default percentage of total width allocated to the tree constituent component. */
-  private double TREE_WIDTH = 0.4d; 
-  
-  /** default percentage of total height allocated to the tree constituent component. */
-  private double TREE_HEIGHT = 0.7d; 
+	/** total height of the componment */
+	private int HEIGHT = 768;
 
-  /** Configuration defining the "query space" this editor encompasses. */
-  private MartConfiguration martConfiguration;
+	/** total width of the component */
+	private int WIDTH = 1024;
 
-  /** The query part of the model. */
-  private Query query;
+	/** default percentage of total width allocated to the tree constituent component. */
+	private double TREE_WIDTH = 0.4d;
 
-  private DefaultTreeModel treeModel;
-  private DefaultMutableTreeNode rootNode;
-  
-  private JTree treeView;
-  private JPanel inputPanel;
-  private JPanel outputPanel;
-  
-  
-  private DatasetSelectionPage datasetSelectionPage;
-  private Dataset currentDataset;
-  private OutputSettingsPage outputSettingsPage;
+	/** default percentage of total height allocated to the tree constituent component. */
+	private double TREE_HEIGHT = 0.7d;
 
-  public QueryEditor(MartConfiguration config) {
-    this.martConfiguration = config;
-    this.query = new Query();
+	/** Configuration defining the "query space" this editor encompasses. */
+	private MartConfiguration martConfiguration;
 
-    query.addPropertyChangeListener( this );
+	/** The query part of the model. */
+	private Query query;
 
-    initTree();
-    initInputPanel();
-    initOutputPanel();
+	private DefaultTreeModel treeModel;
+	private DefaultMutableTreeNode rootNode;
 
-    initialLayout();
-    
-    addDatasetSelectionPage();
-  }
+	private JTree treeView;
+	private JPanel inputPanel;
+	private JPanel outputPanel;
 
+	private DatasetSelectionPage datasetSelectionPage;
+	private Dataset currentDataset;
+	private OutputSettingsPage outputSettingsPage;
 
-  private void showInputPage( InputPage page ) {
-    ((CardLayout)(inputPanel.getLayout())).show( inputPanel, page.getName() );
-  }
+	private AttributePageSetWidget attributesPage;
 
-  /**
-   * Adds page to input panel and tree view.
-   * @param page page to be added
-   * @param treeIndex index of node from root node.
-   */
-  private void addPage(InputPage page, int treeIndex ) {
-      
-     //  Add page to input panel.
-     inputPanel.add( page.getName(), page );
-     showInputPage( page );
-    
-     // Add page's node and show it
-     treeModel.insertNodeInto( page.getNode(), rootNode, treeIndex );
-     TreePath path = new TreePath( rootNode ).pathByAddingChild( page.getNode() );
-     treeView.makeVisible(  path ); 
-    
-     treeView.setRootVisible( false );
-  }
+	public QueryEditor(MartConfiguration config) {
+		this.martConfiguration = config;
+		this.query = new Query();
 
-  /**
-   * Adds the Dataset selection page to the panel.
-   */
-  private void addDatasetSelectionPage() {
-    
-    datasetSelectionPage = new DatasetSelectionPage(query, martConfiguration );
-    currentDataset = null;
-    addPage( datasetSelectionPage, 0);
-  }
+		query.addPropertyChangeListener(this);
 
+		initTree();
+		initInputPanel();
+		initOutputPanel();
 
-  /**
-   * Adds attribute, filter and output settings pages to panel. 
-   */
-  private void addAttributePages( Dataset dataset ) {
-    AttributePageSetWidget attributesPage = new AttributePageSetWidget( query, dataset );
-    addPage( attributesPage, 1 );
-  }
-  
-  
-  private void addFilterPages() {
-    
-  }
-  
-  private void addOutputSettingsPage(){
-    outputSettingsPage = new OutputSettingsPage();
-    outputSettingsPage.addPropertyChangeListener( this );
-    
-    addPage( outputSettingsPage, 2 );
-  }
+		initialLayout();
 
+		addDatasetSelectionPage();
+	}
 
-
-  /**
-   * Sets the prefered sizes for constituent components and adds them
-   * to this component.
-   * All sizes are relative to the treeview dimensions. Layout is:
-   * <pre>
-   * tree   |    input
-   * -----------------
-   *     output
-   * </pre>
-   */
-  private void initialLayout() {
-
-    int topHeight = (int) (HEIGHT * TREE_HEIGHT);
-    int treeWidth = (int) (WIDTH * TREE_WIDTH);
-    int inputWidth = WIDTH - treeWidth;
-    int outputHeight = HEIGHT - topHeight;
-    
-    treeView.setPreferredSize(new Dimension(treeWidth, treeWidth));
-    inputPanel.setPreferredSize(new Dimension(inputWidth, topHeight));
-    outputPanel.setPreferredSize(new Dimension(WIDTH, outputHeight));
-
-    JSplitPane top =
-      new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, new JScrollPane(treeView), new JScrollPane(inputPanel) );
-    top.setOneTouchExpandable(true);
-    JSplitPane topAndBottom =
-      new JSplitPane(JSplitPane.VERTICAL_SPLIT, top, new JScrollPane(outputPanel) );
-    topAndBottom.setOneTouchExpandable(true);
-    add(topAndBottom);
-  }
-
-  /**
-   * 
-   */
-  private void initOutputPanel() {
-    outputPanel = new JPanel();
-    outputPanel.add(new JLabel("output panel"));
-  }
-
-  /**
-   * 
-   */
-  private void initInputPanel() {
-    inputPanel = new JPanel();
-    inputPanel.setLayout(new CardLayout());
-    inputPanel.add("input", new JLabel("input panel"));
-  }
-
-  /**
-   * 
-   */
-  private void initTree() {
-    rootNode = new DefaultMutableTreeNode( "Query" );
-    treeModel = new DefaultTreeModel( rootNode );
-    treeView = new JTree( treeModel );
-    treeView.addTreeSelectionListener( this );
-  }
-
-  public static void main(String[] args) throws ConfigurationException {
-    String confFile = "data/XML/MartConfigurationTemplate.xml";
-    URL confURL = ClassLoader.getSystemResource(confFile);
-    MartConfiguration config =
-      new MartConfigurationFactory().getInstance(confURL);
-
-    QueryEditor editor = new QueryEditor(config);
-    JFrame f = new JFrame("Query Editor");
-    f.getContentPane().add(editor);
-    f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    f.pack();
-    f.setVisible(true);
-  }
+	private void showInputPage(InputPage page) {
+		((CardLayout) (inputPanel.getLayout())).show(inputPanel, page.getName());
+	}
 
 	/**
-   * Redraws the tree if there are any property changes in the Query.
+	 * Adds page to input panel and tree view.
+	 * @param page page to be added
+	 */
+	private void addPage(InputPage page) {
+
+		//  Add page to input panel.
+		inputPanel.add(page.getName(), page);
+		showInputPage(page);
+
+		// Add page's node and show it
+		treeModel.insertNodeInto(
+			page.getNode(),
+			rootNode,
+			rootNode.getChildCount());
+		TreePath path = new TreePath(rootNode).pathByAddingChild(page.getNode());
+		treeView.makeVisible(path);
+
+		treeView.setRootVisible(false);
+	}
+
+	/**
+	 * Adds the Dataset selection page to the panel.
+	 */
+	private void addDatasetSelectionPage() {
+
+		datasetSelectionPage = new DatasetSelectionPage(query, martConfiguration);
+		currentDataset = null;
+		addPage(datasetSelectionPage);
+	}
+
+	/**
+	 * Sets the prefered sizes for constituent components and adds them
+	 * to this component.
+	 * All sizes are relative to the treeview dimensions. Layout is:
+	 * <pre>
+	 * tree   |    input
+	 * -----------------
+	 *     output
+	 * </pre>
+	 */
+	private void initialLayout() {
+
+		int topHeight = (int) (HEIGHT * TREE_HEIGHT);
+		int treeWidth = (int) (WIDTH * TREE_WIDTH);
+		int inputWidth = WIDTH - treeWidth;
+		int outputHeight = HEIGHT - topHeight;
+
+		treeView.setPreferredSize(new Dimension(treeWidth, treeWidth));
+		inputPanel.setPreferredSize(new Dimension(inputWidth, topHeight));
+		outputPanel.setPreferredSize(new Dimension(WIDTH, outputHeight));
+
+		JSplitPane top =
+			new JSplitPane(
+				JSplitPane.HORIZONTAL_SPLIT,
+				new JScrollPane(treeView),
+				new JScrollPane(inputPanel));
+		top.setOneTouchExpandable(true);
+		JSplitPane topAndBottom =
+			new JSplitPane(
+				JSplitPane.VERTICAL_SPLIT,
+				top,
+				new JScrollPane(outputPanel));
+		topAndBottom.setOneTouchExpandable(true);
+		add(topAndBottom);
+	}
+
+	/**
+	 * 
+	 */
+	private void initOutputPanel() {
+		outputPanel = new JPanel();
+		outputPanel.add(new JLabel("output panel"));
+	}
+
+	/**
+	 * 
+	 */
+	private void initInputPanel() {
+		inputPanel = new JPanel();
+		inputPanel.setLayout(new CardLayout());
+		inputPanel.add("input", new JLabel("input panel"));
+	}
+
+	/**
+	 * 
+	 */
+	private void initTree() {
+		rootNode = new DefaultMutableTreeNode("Query");
+		treeModel = new DefaultTreeModel(rootNode);
+		treeView = new JTree(treeModel);
+		treeView.addTreeSelectionListener(this);
+	}
+
+	public static void main(String[] args) throws ConfigurationException {
+		String confFile = "data/XML/MartConfigurationTemplate.xml";
+		URL confURL = ClassLoader.getSystemResource(confFile);
+		MartConfiguration config =
+			new MartConfigurationFactory().getInstance(confURL);
+
+		QueryEditor editor = new QueryEditor(config);
+		JFrame f = new JFrame("Query Editor");
+		f.getContentPane().add(editor);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.pack();
+		f.setVisible(true);
+	}
+
+	/**
+	 * Redraws the tree if there are any property changes in the Query.
 	 * @see java.beans.PropertyChangeListener#propertyChange(java.beans.PropertyChangeEvent)
 	 */
 	public void propertyChange(PropertyChangeEvent evt) {
-		
-    if ( evt.getSource()==query ) {
-      
-      String propertyName = evt.getPropertyName();
-      
-      // update pages if dataset changed.
-      if ( currentDataset!=datasetSelectionPage.getSelectedDataset() ) {
-              
-        if ( query.getStarBases()!=null && query.getPrimaryKeys()!=null ) {
-        
-          currentDataset =  datasetSelectionPage.getSelectedDataset();
-          
-          addAttributePages( currentDataset );   
-          addFilterPages();
-          addOutputSettingsPage();
-        
-           
-        }     
-      }
-      
-      Enumeration enum = rootNode.breadthFirstEnumeration();
-      while ( enum.hasMoreElements() ) 
-        treeModel.nodeChanged( (TreeNode)enum.nextElement() );
-    
-      System.out.println( "Query Changed: " + query );    
-    }
-    
-    if ( evt.getSource()==outputSettingsPage ) {
-      System.out.println( "Output changed: " );
-      treeModel.nodeChanged( outputSettingsPage.getNode() );
-    }
-    
-	}
-  
- 
 
+		if (evt.getSource() == query) {
+
+			String propertyName = evt.getPropertyName();
+
+			// update pages if dataset changed.
+			if (currentDataset != datasetSelectionPage.getSelectedDataset())
+				updateModelAndViewAfterDatasetChanged();
+
+			Enumeration enum = rootNode.breadthFirstEnumeration();
+			while (enum.hasMoreElements())
+				treeModel.nodeChanged((TreeNode) enum.nextElement());
+
+			System.out.println("Query Changed: " + query);
+		}
+
+		if (evt.getSource() == outputSettingsPage) {
+			treeModel.nodeChanged(outputSettingsPage.getNode());
+		}
+
+	}
 
 	/**
-   * Show input page corresponding to selected tree node. 
+	 * Update the model (query) and the view (tree and inputPanels).
 	 */
-	public void valueChanged(TreeSelectionEvent e) {
-     DefaultMutableTreeNode node = (DefaultMutableTreeNode)e.getNewLeadSelectionPath().getLastPathComponent(); 
-		 InputPage page = (InputPage) node.getUserObject();
-     showInputPage( page );
+	private void updateModelAndViewAfterDatasetChanged() {
+
+		// Basically we remove most things from the model and views before adding those things that should
+		// be present back in.
+
+		// Update the model
+		query.removeAllAttributes();
+		query.removeAllFilters();
+
+		// enables soon to be obsolete listeners to be garbage collected once they are removed from the views.
+		query.removeAllPropertyChangeListeners();
+		// but we still need to listen to changes keep tree up to date
+		query.addPropertyChangeListener(this);
+
+		// We need to remove all pages from the inputPanel so that we can add pages with the same
+		// name again later. e.g. attributesPage.
+		inputPanel.removeAll();
+
+		// Remove all nodes from the tree then add the datasetSelectionPage. Removing
+		// the other nodes one at a time using treeModel.removeNodeFromParent( XXX.getNode() ) 
+		// failed to work on jdk1.4.1@linux. 
+		rootNode.removeAllChildren();
+		addPage(datasetSelectionPage);
+		treeModel.reload();
+
+		currentDataset = datasetSelectionPage.getSelectedDataset();
+
+		// Add attribute, filter and output pages to the views.
+		attributesPage = new AttributePageSetWidget(query, currentDataset);
+		addPage(attributesPage);
+		// todo add filter page here
+		outputSettingsPage = new OutputSettingsPage();
+		outputSettingsPage.addPropertyChangeListener(this);
+		addPage(outputSettingsPage);
+
 	}
 
+	/**
+	 * Show input page corresponding to selected tree node. 
+	 */
+	public void valueChanged(TreeSelectionEvent e) {
+		if (e.getNewLeadSelectionPath() != null
+			&& e.getNewLeadSelectionPath().getLastPathComponent() != null) {
 
+			DefaultMutableTreeNode node =
+				(DefaultMutableTreeNode) e
+					.getNewLeadSelectionPath()
+					.getLastPathComponent();
+			InputPage page = (InputPage) node.getUserObject();
+			showInputPage(page);
+		}
+	}
 
-  /**
-   * @return
-   */
-  public MartConfiguration getMartConfiguration() {
-    return martConfiguration;
-  }
+	/**
+	 * @return
+	 */
+	public MartConfiguration getMartConfiguration() {
+		return martConfiguration;
+	}
 
 }
