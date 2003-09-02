6e941bbeb53a031a04ac7f88de6c92783a208f7d
craig <>
2003-09-02 16:05:38 +0000

    Updaded martgui.* to (temporarilly) launch the QueryEditor.

--- bin/martgui.bat
+++ bin/martgui.bat
@@ -1,1 +1,4 @@
-java -cp ..\lib\martj.jar;..\lib\log4j-1.2.6.jar;..\lib\ecp1_0beta.jar;..\lib\mysql-connector-java-3.0.7-stable-bin.jar;..\lib\ensj-util.jar;..\lib\jython.jar org.ensembl.mart.explorer.Runner
+
+echo "This is a partial DEMO of MartExplorer. It demonstrates part of the application, the query editor, rather than the full application."
+
+java -cp ..\build\classes;..\lib\martj.jar;..\lib\log4j-1.2.6.jar;..\lib\ecp1_0beta.jar;..\lib\mysql-connector-java-3.0.7-stable-bin.jar;..\lib\ensj-util.jar;..\lib\jdom.jar  org.ensembl.mart.explorer.QueryEditor 
--- bin/martgui.sh
+++ bin/martgui.sh
@@ -10,25 +10,18 @@
 # prompt> bin/martgui.sh
 TMP_ROOT=`dirname $0`/..
 
-if [ -s ${TMP_ROOT}/src/jython/martexplorer/MartExplorerGUIApplication.py ] 
-then
-  CACHE_DIR=${HOME}/.martshell_cachedir
+TMP_ROOT=`dirname $0`/..
  
-  TMP_ROOT=`dirname $0`/..
- 
-  TMP_CLASSPATH=${TMP_ROOT}/build/classes
-  TMP_CLASSPATH=${TMP_CLASSPATH}:${TMP_ROOT}/lib/mysql-connector-java-3.0.7-stable-bin.jar
-  TMP_CLASSPATH=${TMP_CLASSPATH}:${TMP_ROOT}/lib/log4j-1.2.6.jar
-  TMP_CLASSPATH=${TMP_CLASSPATH}:${TMP_ROOT}/lib/jython.jar
-  TMP_CLASSPATH=${TMP_CLASSPATH}:${TMP_ROOT}/lib/ensj-util.jar
-  TMP_CLASSPATH=${TMP_CLASSPATH}:${TMP_ROOT}/lib/jdom.jar 
+TMP_CLASSPATH=${TMP_ROOT}/build/classes
+TMP_CLASSPATH=${TMP_CLASSPATH}:${TMP_ROOT}/lib/mysql-connector-java-3.0.7-stable-bin.jar
+TMP_CLASSPATH=${TMP_CLASSPATH}:${TMP_ROOT}/lib/log4j-1.2.6.jar
+TMP_CLASSPATH=${TMP_CLASSPATH}:${TMP_ROOT}/lib/jython.jar
+TMP_CLASSPATH=${TMP_CLASSPATH}:${TMP_ROOT}/lib/ensj-util.jar
+TMP_CLASSPATH=${TMP_CLASSPATH}:${TMP_ROOT}/lib/jdom.jar
+TMP_CLASSPATH=${TMP_CLASSPATH}:${TMP_ROOT}/lib/martj.jar 
 
-  TMP_JYTHON_LIB=${TMP_ROOT}/lib
- 
-  CMD="java -classpath ${TMP_CLASSPATH} -Dpython.cachedir=${CACHE_DIR} -Dpython.path=${TMP_JYTHON_LIB} org.python.util.jython ${TMP_ROOT}/src/jython/martexplorer/MartExplorerGUIApplication.py $@"
- 
-  #echo $CMD
-  $CMD
-else 
-  java -jar ${TMP_ROOT}/lib/martj.jar $@
-fi
+TMP_JYTHON_LIB=${TMP_ROOT}/lib
+
+echo "This is a partial DEMO of MartExplorer. It demonstrates part of the application, the query editor, rather than the full application."
+java org.ensembl.mart.explorer.QueryEditor $@
+
