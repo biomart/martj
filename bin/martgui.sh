80ffa94dcc2f6e57409c20dc71a4d1e100cdf01a
arek <arek@sanger.ac.uk>
2003-09-02 16:40:11 +0000

    now includes classpath

--- bin/martgui.sh
+++ bin/martgui.sh
@@ -12,7 +12,8 @@ TMP_ROOT=`dirname $0`/..
 
 TMP_ROOT=`dirname $0`/..
  
-TMP_CLASSPATH=${TMP_ROOT}/build/classes
+TMP_CLASSPATH=${TMP_ROOT}
+TMP_CLASSPATH=${TMP_CLASSPATH}:${TMP_ROOT}/build/classes
 TMP_CLASSPATH=${TMP_CLASSPATH}:${TMP_ROOT}/lib/mysql-connector-java-3.0.7-stable-bin.jar
 TMP_CLASSPATH=${TMP_CLASSPATH}:${TMP_ROOT}/lib/log4j-1.2.6.jar
 TMP_CLASSPATH=${TMP_CLASSPATH}:${TMP_ROOT}/lib/jython.jar
@@ -23,5 +24,8 @@ TMP_CLASSPATH=${TMP_CLASSPATH}:${TMP_ROOT}/lib/martj.jar
 TMP_JYTHON_LIB=${TMP_ROOT}/lib
 
 echo "This is a partial DEMO of MartExplorer. It demonstrates part of the application, the query editor, rather than the full application."
-java org.ensembl.mart.explorer.QueryEditor $@
+java -cp $TMP_CLASSPATH org.ensembl.mart.explorer.QueryEditor $@
+
+
+
 
