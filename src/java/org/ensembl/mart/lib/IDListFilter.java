6548d76fcc0544b411dddae8dec1b6034d94b308
dlondon <>
2003-09-15 18:01:04 +0000

    added support for handlers, over rides the default handlers if a user supplies a handler in the xml.  User is responsible for writing code to properly handle this.

--- src/java/org/ensembl/mart/lib/IDListFilter.java
+++ src/java/org/ensembl/mart/lib/IDListFilter.java
@@ -47,7 +47,7 @@ public class IDListFilter implements Filter {
 	public static final String STRING = "org.ensembl.mart.lib.StringIDListFilterHandler";
 	public static final String FILE = "org.ensembl.mart.lib.FileIDListFilterHandler";
 	public static final String URL = "org.ensembl.mart.lib.URLIDListFilterHandler";
-	public static  final String SUBQUERY = "org.ensembl.mart.lib.SubQueryIDListFilterHandler";
+	public static final String SUBQUERY = "org.ensembl.mart.lib.SubQueryIDListFilterHandler";
 
 	/**
 	 * Construct an STRING type IDListFilter object of a given field name on a String[] List of 
@@ -55,9 +55,10 @@ public class IDListFilter implements Filter {
 	 * 
 	 * @param String name - field name
 	 * @param String[] identifiers
+   * @see StringIDListFilterHandler
 	 */
 	public IDListFilter(String field, String[] identifiers) {
-		this(field, null, identifiers);
+		this(field, null, identifiers, null);
 	}
 
 	/**
@@ -67,27 +68,48 @@ public class IDListFilter implements Filter {
 	 * @param String name - field name
 	 * @param String tableConstraint - table constraint for field name
 	 * @param String[] identifiers
+   * @see StringIDListFilterHandler
 	 */
 	public IDListFilter(String field, String tableConstraint, String[] identifiers) {
+		this(field, tableConstraint, identifiers, null);
+	}
+
+	/**
+	 * Construct an IDListFilter object of a given field name and tableConstraint, on a String[] List of 
+	 * identifiers, with a user supplied handler (default handler for this type of IDListFilter is IDListFilter.STRING).
+	 * 
+	 * @param String name - field name
+	 * @param String tableConstraint - table constraint for field name
+	 * @param String[] identifiers
+   * @param handler -- handler object to process this Filter, default is used if null
+   * @see StringIDListFilterHandler
+	 */
+  public IDListFilter(String field, String tableConstraint, String[] identifiers, String handler) {
 		this.field = field;
 		this.tableConstraint = tableConstraint;
 		this.identifiers = Arrays.asList(identifiers);
-    this.file = null;
-    this.url = null;
-    this.subQuery = null;
-		handler = STRING;
-    setHashCode();
-	}
-
+		this.file = null;
+		this.url = null;
+		this.subQuery = null;
+		
+		if (handler != null)
+		  this.handler = handler;
+		else
+		  this.handler = STRING;
+		
+		setHashCode();
+  }
+  
 	/**
 	 * Construct a FILE type IDListFilter object of a given field name from a file
 	 * containing identifiers.
 	 * 
 	 * @param String name - field name
 	 * @param File file
+   * @see FileIDListFilterHandler
 	 */
 	public IDListFilter(String field, File file) {
-		this(field, null, file);
+		this(field, null, file, null);
 	}
 
 	/**
@@ -97,26 +119,48 @@ public class IDListFilter implements Filter {
 	 * @param String name - field name
 	 * @param String tableConstraint - table constraint for field name
 	 * @param File file
+   * @see FileIDListFilterHandler
 	 */
 	public IDListFilter(String name, String tableConstraint, File file) {
+     this(name, tableConstraint, file, null);
+	}
+
+	/**
+	 * Construct a FILE type IDListFilter object of a given field name from a file
+	 * containing identifiers, with a user supplied handler (the default for this type of
+	 * IDListFilter is IDListFilter.FILE).
+	 * 
+	 * @param String name - field name
+	 * @param String tableConstraint - table constraint for field name
+	 * @param File file
+   * @param handler -- handler object to process this Filter, default is used if null
+   * @see FileIDListFilterHandler
+	 */
+  public IDListFilter(String name, String tableConstraint, File file, String handler) {
 		this.field = name;
 		this.tableConstraint = tableConstraint;
 		this.file = file;
-    this.url = null;
-    this.subQuery = null;
-		handler = FILE;
-    setHashCode();
-	}
-
+		this.url = null;
+		this.subQuery = null;
+		
+		if (handler != null)
+		  this.handler = handler;
+		else
+		  this.handler = FILE;
+		  
+		setHashCode();
+  }
+  
 	/**
 	 * Construct a URL type IDListFilter object of a given field name 
 	 * from a specified URL object containing identifiers.  
 	 * 
 	 * @param String name - field name
 	 * @param URL url
+   * @see URLIDListFilterHandler
 	 */
 	public IDListFilter(String name, URL url) {
-		this(name, null, url);
+		this(name, null, url, null);
 	}
 
 	/**
@@ -126,17 +170,35 @@ public class IDListFilter implements Filter {
 	 * @param String name - field name
 	 * @param String tableConstraint - table constraint for field name
 	 * @param URL url - url pointing to resource with IDs
+   * @see URLIDListFilterHandler
 	 */
 	public IDListFilter(String name, String tableConstraint, URL url) {
+    this (name, tableConstraint, url, null);
+	}
+
+  /**
+   * Construct a URL type IDListFilter, with a given fieldName and tableConstraint, with
+   * a user supplied handler (default handler for this type of Filter is IDListFilter.URL)
+	 * @param String name - field name
+	 * @param String tableConstraint - table constraint for field name
+	 * @param URL url - url pointing to resource with IDs
+   * @param handler -- handler object to process this Filter, default is used if null
+   * @see URLIDListFilterHandler
+   */
+  public IDListFilter(String name, String tableConstraint, URL url, String handler) {
 		this.field = name;
 		this.tableConstraint = tableConstraint;
 		this.url = url;
-    this.file = null;
-    this.subQuery = null;
-		handler = URL;
-    setHashCode();
-	}
-
+		this.file = null;
+		this.subQuery = null;
+		
+		if (handler != null)
+		  this.handler = handler;
+		else  
+		  this.handler = URL;
+		setHashCode();
+  }
+  
 	/**
 	 * Construct a SUBQUERY type IDListFilter object of a given field name
 	 * from a Query object that fits the constraints of a subQuery.
@@ -145,7 +207,7 @@ public class IDListFilter implements Filter {
 	 * @see QueryIDListFilterHandler 
 	 */
 	public IDListFilter(String name, Query subQuery) {
-		this(name, null, subQuery);
+		this(name, null, subQuery, null);
 	}
 
 	/**
@@ -158,40 +220,56 @@ public class IDListFilter implements Filter {
 	 * @see QueryIDListFilterHandler 
 	 */
 	public IDListFilter(String name, String tableConstraint, Query subQuery) {
+		this(name, tableConstraint, subQuery, null);
+	}
+
+  /**
+   * Construct a SUBQUERY type IDListFilter object of a given field name, tableConstraint, with
+   * a user supplied handler (default handler for this type of Filter is IDListFilter.SUBQUERY)
+	 * @param String name - field name
+	 * @param String tableConstraint - table constraint for field name
+	 * @param Query subQuery - Query that, when evaluated, returns a list of IDs
+   * @param handler -- handler object to process this Filter, default is used if null
+ 	 * @see QueryIDListFilterHandler
+   */
+  public IDListFilter(String name, String tableConstraint, Query subQuery, String handler) {
 		this.field = name;
 		this.tableConstraint = tableConstraint;
 		this.subQuery = subQuery;
-    this.file = null;
-    this.url = null;
-		handler = SUBQUERY;
-    setHashCode();
-	}
+		this.file = null;
+		this.url = null;
+		
+		if (handler != null)
+		  this.handler = handler;
+		else
+		  this.handler = SUBQUERY;
+		setHashCode();
+  }
+  
+	private void setHashCode() {
+		hashcode = (field == null) ? 0 : field.hashCode();
+		hashcode = (tableConstraint != null) ? (31 * hashcode) + tableConstraint.hashCode() : hashcode;
+		hashcode = (handler != null) ? (31 * hashcode) + handler.hashCode() : hashcode;
 
-  private void setHashCode() {
-    hashcode = (field == null) ? 0 : field.hashCode();
-    hashcode = (tableConstraint != null) ? (31 * hashcode) + tableConstraint.hashCode() : hashcode;
-    hashcode = (handler != null) ? (31 * hashcode) + handler.hashCode() : hashcode;
-    
-    if (handler == null || STRING.equals(handler))
+		if (identifiers.size() > 0) {
 			for (int i = 0, n = identifiers.size(); i < n; i++) {
 				String element = (String) identifiers.get(i);
 				hashcode = (31 * hashcode) + element.hashCode();
 			}
-    else if (FILE.equals(handler))
-		  hashcode = (31 * hashcode) + file.hashCode();
-    else if (URL.equals(handler))
-		  hashcode = (31 * hashcode) + url.hashCode();
-    else
-		  hashcode = (31 * hashcode) + subQuery.hashCode();
-  }
-  
+		}
+
+		hashcode = (file != null) ? (31 * hashcode) + file.hashCode() : hashcode;
+		hashcode = (url != null) ? (31 * hashcode) + url.hashCode() : hashcode;
+		hashcode = (subQuery != null) ? (31 * hashcode) + subQuery.hashCode() : hashcode;
+	}
+
 	/**
 	 * returns the Where Clause for the SQL
 	 * 
 	 * @return String where clause 'IN (quoted list of identifiers)'
 	 * @throws InvalidListException when IDListFilter is not a STRING type.
 	 */
-	public String getWhereClause() {			
+	public String getWhereClause() {
 		StringBuffer buf = new StringBuffer();
 		buf.append(field).append(" IN (");
 		for (int i = 0, n = identifiers.size(); i < n; i++) {
@@ -209,7 +287,7 @@ public class IDListFilter implements Filter {
 	 * same as getWhereClause()
 	 */
 	public String getRightHandClause() {
-		  
+
 		StringBuffer buf = new StringBuffer();
 		buf.append(" IN (");
 		for (int i = 0, n = identifiers.size(); i < n; i++) {
@@ -246,10 +324,10 @@ public class IDListFilter implements Filter {
 		return null;
 	}
 
-  /**
-   * Returns the tableConstraint for this IDListFilter Object
-   * @return String tableConstriant
-   */
+	/**
+	 * Returns the tableConstraint for this IDListFilter Object
+	 * @return String tableConstriant
+	 */
 	public String getTableConstraint() {
 		return tableConstraint;
 	}
@@ -304,16 +382,12 @@ public class IDListFilter implements Filter {
 		buf.append("[");
 		buf.append(" field=").append(field);
 		buf.append(", tableConstraint=").append(tableConstraint);
+		buf.append(", handler=").append(handler);
+		buf.append(", identifiers=").append(identifiers);
+		buf.append(", File=").append(file);
+		buf.append(", URL=").append(url);
+		buf.append(", Query=").append(subQuery);
 
-	 if (handler == null || STRING.equals(handler))
-				buf.append(", identifiers=").append(identifiers);
-   else if (FILE.equals(handler))
-				buf.append(", File=").append(file);
-   else if (URL.equals(handler))
-				buf.append(", URL=").append(url);
-	 else
-				buf.append(", Query=").append(subQuery);
-	
 		buf.append("]");
 
 		return buf.toString();
@@ -334,14 +408,14 @@ public class IDListFilter implements Filter {
 	}
 
 	private final String field;
-  private final String tableConstraint;
-  private final String handler;
-  
+	private final String tableConstraint;
+	private final String handler;
+
 	private final Query subQuery; // for Query based Filter
 	private final File file;
 	private final URL url;
-	
+
 	private List identifiers = new ArrayList();
-  
-  private int hashcode = 0; //hashcode for immutable object
+
+	private int hashcode = 0; //hashcode for immutable object
 }
