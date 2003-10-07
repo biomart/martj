5a4023b5ec542067e79f9cf9e18b4ef0408c1327
craig <>
2003-10-07 17:24:24 +0000

    Test class for CompositeDSViewAdaptor.

---
+++ src/java/org/ensembl/mart/lib/config/test/CompositeDSViewAdaptorTest.java
@@ -0,0 +1,72 @@
+/*
+	Copyright (C) 2003 EBI, GRL
+
+	This library is free software; you can redistribute it and/or
+	modify it under the terms of the GNU Lesser General Public
+	License as published by the Free Software Foundation; either
+	version 2.1 of the License, or (at your option) any later version.
+
+	This library is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+	Lesser General Public License for more details.
+
+	You should have received a copy of the GNU Lesser General Public
+	License along with this library; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+package org.ensembl.mart.lib.config.test;
+
+import junit.framework.TestCase;
+
+import org.ensembl.mart.lib.config.CompositeDSViewAdaptor;
+import org.ensembl.mart.lib.config.DatasetView;
+import org.ensembl.mart.lib.config.URLDSViewAdaptor;
+
+/**
+ * Tests CompositeDSViewAdaptor.
+ */
+public class CompositeDSViewAdaptorTest extends TestCase {
+
+
+  public void testAll() throws Exception {
+  
+    CompositeDSViewAdaptor adaptor = new CompositeDSViewAdaptor();
+  
+    assertTrue( adaptor.getDatasetDisplayNames().length==0 );
+    assertTrue( adaptor.getDatasetViews().length==0 );
+    
+    URLDSViewAdaptor urlAdaptor = URLDSViewAdaptorTest.getSampleDSViewAdaptor();
+    
+    adaptor.add( urlAdaptor );
+    assertTrue( adaptor.getDatasetViews().length==1 );
+    assertTrue( adaptor.getDatasetDisplayNames().length==1 );
+    DatasetView view = adaptor.getDatasetViews()[0];
+    assertNotNull( view );
+    assertTrue( view.getAllFilterDescriptions().size()>0 );
+    
+    assertTrue( adaptor.remove( urlAdaptor ) );
+    assertTrue( adaptor.getDatasetDisplayNames().length==0 );
+    assertTrue( adaptor.getDatasetViews().length==0 );
+
+    adaptor.add( urlAdaptor );
+    adaptor.clear();
+    assertTrue( adaptor.getDatasetDisplayNames().length==0 );
+    assertTrue( adaptor.getDatasetViews().length==0 );
+      
+  }
+
+	/**
+	 * Constructor for CompositeDSViewAdaptorTest.
+	 * @param arg0
+	 */
+	public CompositeDSViewAdaptorTest(String arg0) {
+		super(arg0);
+	}
+
+	public static void main(String[] args) {
+		junit.textui.TestRunner.run(CompositeDSViewAdaptorTest.class);
+	}
+
+}
