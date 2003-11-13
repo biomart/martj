0b113ae843cb714430d3a4038cc4dcfd73b98b64
dlondon <>
2003-11-13 09:59:12 +0000

    martshell.sh ----------- removed spurious jar files
    marthsell.bat
    -----------
    processes arguments correctly
    TMP_CLASSPATH setup as in martshell.sh -- easier to keep in sync

--- bin/martshell.bat
+++ bin/martshell.bat
@@ -1,1 +1,28 @@
-java -cp "..\;..\build\classes;..\lib\libreadline-java.jar;..\lib\martj.jar;..\lib\log4j-1.2.6.jar;..\lib\ecp1_0beta.jar;..\lib\mysql-connector-java-3.0.7-stable-bin.jar;..\lib\ensj-util.jar;..\lib\jython.jar;..\lib\java-getopt-1.0.9.jar;..\lib\jdom.jar" org.ensembl.mart.shell.MartShell %1 %2 %3 %4 %5 %6 %7 %8 %9
+REM CLASSPATH
+SET TMP_ROOT=..
+SET TMP_CLASSPATH=%TMP_ROOT%
+SET TMP_CLASSPATH=%TMP_CLASSPATH%:%TMP_ROOT%/build/classes 
+SET TMP_CLASSPATH=%TMP_CLASSPATH%:%TMP_ROOT%/lib/martj.jar
+SET TMP_CLASSPATH=%TMP_CLASSPATH%:%TMP_ROOT%/lib/mysql-connector-java-3.0.7-stable-bin.jar
+SET TMP_CLASSPATH=%TMP_CLASSPATH%:%TMP_ROOT%/lib/java-getopt-1.0.9.jar
+SET TMP_CLASSPATH=%TMP_CLASSPATH%:%TMP_ROOT%/lib/jdom.jar
+SET TMP_CLASSPATH=%TMP_CLASSPATH%:%TMP_ROOT%/lib/libreadline-java.jar
+SET TMP_CLASSPATH=%TMP_CLASSPATH%:%TMP_ROOT%/lib/ensj-util.jar
+SET TMP_CLASSPATH=%TMP_CLASSPATH%:%TMP_ROOT%/lib/ecp1_0beta.jar
+SET TMP_CLASSPATH=%TMP_CLASSPATH%:%TMP_ROOT%/lib/jdbc2_0-stdext.jar
+
+REM BUILD UP A COMMAND WITH ANY ARGUMENTS PASSED
+SET com=org.ensembl.mart.shell.MartShell
+
+ECHO off
+REM ECHO %com%
+
+:ARGUMENT
+SHIFT
+SET test=%0
+IF "%0" == "" GOTO PROCESS
+SET com=%com% %test%
+GOTO ARGUMENT
+
+:PROCESS
+java -cp %TMP_CLASSPATH %com%
