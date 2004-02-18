44c818d541794cd1402d8e3af1ecda463f2e9054
craig <>
2004-02-18 14:33:03 +0000

    Now launches MartExplorer.

--- bin/martgui.bat
+++ bin/martgui.bat
@@ -1,4 +1,4 @@
 
 echo "This is a partial DEMO of MartExplorer. It demonstrates part of the application, the query editor, rather than the full application."
 
-java -cp ..\build\classes;..\lib\martj.jar;..\lib\log4j-1.2.6.jar;..\lib\ecp1_0beta.jar;..\lib\mysql-connector-java-3.0.7-stable-bin.jar;..\lib\ensj-util.jar;..\lib\jdom.jar  org.ensembl.mart.explorer.QueryEditor 
+java -cp ..\build\classes;..\lib\martj.jar;..\lib\log4j-1.2.6.jar;..\lib\ecp1_0beta.jar;..\lib\mysql-connector-java-3.0.7-stable-bin.jar;..\lib\ensj-util.jar;..\lib\jdom.jar  org.ensembl.mart.explorer.MartExplorer 
