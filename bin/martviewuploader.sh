4baaed2fa357b3c2c3a8ada6d6a7f28a8a558373
craig <>
2004-02-27 14:45:54 +0000

    script for uploading dataset views to database.

---
+++ bin/martviewuploader.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# Note: /bin/sh doesn't work on Alphas (need to use bash thexre) but
+# works everywhere else.
+
+# Starts the MartExplorer GUI application.
+
+# Usage:
+#
+# prompt> bin/martgui.sh
+TMP_ROOT=`dirname $0`/..
+
+TMP_ROOT=`dirname $0`/..
+ 
+TMP_CLASSPATH=${TMP_ROOT}
+TMP_CLASSPATH=${TMP_CLASSPATH}:${TMP_ROOT}/build/classes
+TMP_CLASSPATH=${TMP_CLASSPATH}:${TMP_ROOT}/lib/mysql-connector-java-3.0.7-stable-bin.jar
+TMP_CLASSPATH=${TMP_CLASSPATH}:${TMP_ROOT}/lib/log4j-1.2.6.jar
+TMP_CLASSPATH=${TMP_CLASSPATH}:${TMP_ROOT}/lib/jython.jar
+TMP_CLASSPATH=${TMP_CLASSPATH}:${TMP_ROOT}/lib/ensj-util.jar
+TMP_CLASSPATH=${TMP_CLASSPATH}:${TMP_ROOT}/lib/jdom.jar
+TMP_CLASSPATH=${TMP_CLASSPATH}:${TMP_ROOT}/lib/martj.jar 
+TMP_CLASSPATH=${TMP_CLASSPATH}:${TMP_ROOT}/lib/java-getopt-1.0.9.jar
+
+java -ea -cp $TMP_CLASSPATH org.ensembl.mart.lib.config.DatasetViewUploader $@
+
+
+
+
