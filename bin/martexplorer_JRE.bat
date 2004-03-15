36b27d8b783b21448cfe1c2f5c37cb81913d0f27
dlondon <>
2004-03-15 12:06:15 +0000

    added JRE versions of martshell.bat (never got committed for some reason, and at some point got deleted), and new marteditor and martexplorer JRE sh/bat scripts

---
+++ bin/martexplorer_JRE.bat
@@ -0,0 +1,1 @@
+%TMP_ROOT%/jre/windows/bin/java -ea -cp ..\build\classes;..\lib\martj.jar;..\lib\log4j-1.2.6.jar;..\lib\ecp1_0beta.jar;..\lib\mysql-connector-java-3.0.7-stable-bin.jar;..\lib\ensj-util.jar;..\lib\jdom.jar;..\lib\p6spy.jar  org.ensembl.mart.explorer.MartExplorer 
