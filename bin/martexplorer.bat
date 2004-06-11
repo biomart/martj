f177b5737e0d25603bccd8f46ab2c974a91b77de
arek <arek@sanger.ac.uk>
2004-06-11 10:56:37 +0000

    made MExplorer loggings quiet by default

--- bin/martexplorer.bat
+++ bin/martexplorer.bat
@@ -1,1 +1,1 @@
-java -ea -cp ..\build\classes;..\lib\martj.jar;..\lib\log4j-1.2.6.jar;..\lib\ecp1_0beta.jar;..\lib\mysql-connector-java-3.0.7-stable-bin.jar;..\lib\ensj-util.jar;..\lib\jdom.jar;..\lib\p6spy.jar;..\lib\ojdbc14.jar org.ensembl.mart.explorer.MartExplorer 
+java -Djava.util.logging.config.file=none -ea -cp ..\build\classes;..\lib\martj.jar;..\lib\log4j-1.2.6.jar;..\lib\ecp1_0beta.jar;..\lib\mysql-connector-java-3.0.7-stable-bin.jar;..\lib\ensj-util.jar;..\lib\jdom.jar;..\lib\p6spy.jar;..\lib\ojdbc14.jar org.ensembl.mart.explorer.MartExplorer 
