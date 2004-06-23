11cd564564b999d19a45e1364a2b7d146708290a
craig <>
2004-06-23 14:05:41 +0000

    Added info(...).
    Refactored to use line wrapping code in StringUtil.

--- src/java/org/ensembl/mart/explorer/Feedback.java
+++ src/java/org/ensembl/mart/explorer/Feedback.java
@@ -7,9 +7,10 @@
 package org.ensembl.mart.explorer;
 
 import java.awt.Component;
-import java.util.StringTokenizer;
 
 import javax.swing.JOptionPane;
+
+import org.ensembl.mart.util.StringUtil;
 
 /**
  * @author craig
@@ -19,83 +20,85 @@ import javax.swing.JOptionPane;
  */
 public class Feedback {
 
-	private Component src;
+  private Component src;
 
-	public Feedback(Component src) {
-		this.src = src;
-	}
+  public Feedback(Component src) {
+    this.src = src;
+  }
 
-	
-	/**
-	 * Displays message in warning dialog box.
-	 * @param message
-	 */
-	public void warning(String message) {
-    
-      // split long mesages onto multiple lines with of approximately
-      // max chars
-      StringBuffer buf = new StringBuffer();
-      StringTokenizer tokens = new StringTokenizer(message);
-      int len = 0;
-      int max = 80;
-      while ( tokens.hasMoreTokens() ) {
-        
-        if ( len>max ) {
-          buf.append("\n");
-          len = 0;
-        }
-        if ( len>0 ) buf.append(" ");
-        
-        String token = tokens.nextToken();
-        buf.append( token );
-        len += token.length();
-      }
-    
-			JOptionPane.showMessageDialog(
-				src,
-				buf.toString(),
-				"Warning",
-				JOptionPane.WARNING_MESSAGE);
-		}
+  private void showMessageDialog(
+    String label,
+    String message,
+    int dialogType) {
 
-	/**
-	 * Displays message plus exception.messge in warning dialog box 
-	 * and prints stack trace to console.
+    JOptionPane.showMessageDialog(
+      src,
+      StringUtil.wrapLinesAsHTML(message, 80, true),
+      label,
+      dialogType);
+
+  }
+
+  /**
+   * Displays message in information dialog box.
+   * @param message message to be displayed
+   */
+  public void info(String message) {
+
+    showMessageDialog("Information", message, JOptionPane.INFORMATION_MESSAGE);
+
+  }
+
+  /**
+   * Displays message in warning dialog box.
+   * @param message message to be displayed
+   */
+  public void warning(String message) {
+
+    showMessageDialog("Warning", message, JOptionPane.WARNING_MESSAGE);
+
+  }
+
+  /**
+   * Displays message plus exception.messge in warning dialog box 
+   * and prints stack trace to console.
    * @param message message to display
-   * @param e exception
-	 */
-	public void warning(String message, Exception e) {
-      warning( message, e, true);
-		}
-
+   * @param e exception exception to be displayed
+   */
+  public void warning(String message, Exception e) {
+    warning(message, e, true);
+  }
 
   /**
    * Prints stacktrace and warning message to screen.
    * @param message message to display
-   * @param e exception
+   * @param e exception exception to be displayed
    * @param includeExceptionInWarning whether e.getMessage() should be displayed in warning dialog.
    */
-  public void warning(String message, Exception e, boolean includeExceptionInWarning) {
+  public void warning(
+    String message,
+    Exception e,
+    boolean includeExceptionInWarning) {
     e.printStackTrace();
-    if ( includeExceptionInWarning ) message = message  + ":" + e.getMessage(); 
+    if (includeExceptionInWarning)
+      message = message + ":" + e.getMessage();
     warning(message);
-    
+
   }
 
-		/**
-		 * Displays warning dialog and prints stack trace to console.
-		 * @param e exception
-		 */
-		public void warning(Exception e) {
-			warning(e.getMessage());
-			e.printStackTrace();
-		}
-    
-    public static void main(String[] args) {
-      Feedback f = new Feedback(null);
-      f.warning("A very long test message that goes on and on. It should have been reformated for better display. If not the code needs fixing!");
-    }
+  /**
+   * Displays warning dialog and prints stack trace to console.
+   * @param e exception
+   */
+  public void warning(Exception e) {
+    warning(e.getMessage());
+    e.printStackTrace();
+  }
 
-
+  public static void main(String[] args) {
+    Feedback f = new Feedback(null);
+    f.warning(
+      "A very long test message that goes on and on. It should have been reformated for better display. If not the code needs fixing!");
+  }
 
 }
