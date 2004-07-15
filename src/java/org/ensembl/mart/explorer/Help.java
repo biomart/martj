fa7b7d7f4ebb086fca034ecc3bb91fef61511ed1
craig <>
2004-07-15 18:21:55 +0000

    Implemented hyperlink support, made the scroll bars work properly and made the Help dialog appear in a convenient location when it is shown.

--- src/java/org/ensembl/mart/explorer/Help.java
+++ src/java/org/ensembl/mart/explorer/Help.java
@@ -16,64 +16,102 @@
  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 package org.ensembl.mart.explorer;
+import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.Frame;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-import javax.swing.Box;
-import javax.swing.BoxLayout;
 import javax.swing.JDialog;
 import javax.swing.JEditorPane;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JScrollPane;
+import javax.swing.event.HyperlinkEvent;
+import javax.swing.event.HyperlinkListener;
+import javax.swing.text.html.HTMLDocument;
+import javax.swing.text.html.HTMLFrameHyperlinkEvent;
 
 import org.ensembl.mart.guiutils.QuickFrame;
+
 /**
  * Displays the MartExplorer help file
  * <code>file data/martexplorer_help.html</code>.
  * 
  * @author <a href="mailto:craig@ebi.ac.uk">Craig Melsopp</a>
  * 
- * TODO support hyperlinks
- * TODO wrap text.
  */
-public class Help extends Box {
-	private JEditorPane pane = new JEditorPane();
-	public Help() {
-		super(BoxLayout.Y_AXIS);
-		try {
-			URL url = getClass().getClassLoader().getResource(
-					"data/martexplorer_help.html");
-			pane.setPage(url);
-		} catch (MalformedURLException e) {
-			pane.setText(e.getMessage());
-			e.printStackTrace();
-		} catch (IOException e) {
-			pane.setText(e.getMessage());
-			e.printStackTrace();
-		}
-		add(pane);
-		pane.setEditable(false);
-	}
-	  
-    
-  public void showDialog(Frame parent) {
-    showDialog(parent, JFrame.DISPOSE_ON_CLOSE);
+public class Help extends JEditorPane implements HyperlinkListener {
+
+  private final int WIDTH = 500;
+  private final int HEIGHT = 500;
+
+  public Help() {
+
+    //  enable hyperlinks
+    setEditable(false);
+    addHyperlinkListener(this);
+
+    setPreferredSize(new Dimension(WIDTH, HEIGHT));
+
+    try {
+      URL url =
+        getClass().getClassLoader().getResource("data/martexplorer_help.html");
+      setPage(url);
+
+    } catch (MalformedURLException e) {
+      setText(e.getMessage());
+      e.printStackTrace();
+    } catch (IOException e) {
+      setText(e.getMessage());
+      e.printStackTrace();
+    }
+
   }
-    
-  private void showDialog(Frame parent, int defaultCloseOperation) {
-  JDialog d = new JDialog(parent, "MartExplorer Documentation", false);
-  d.setDefaultCloseOperation(defaultCloseOperation);
-  //d.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-  d.getContentPane().add(new JScrollPane(this));
-  d.setSize(400, 500);
-  d.setVisible(true);
-  } 
-	
+
+  /**
+   * Displays the Help page in a non-modal dialog box.
+   * The dialog is displayed over the bottom right corner
+   * of the parent.
+   * @param parent parent frame
+   */
+  public void showDialog(Frame parent) {
+    JDialog d = new JDialog(parent, "MartExplorer Documentation", false);
+    Container c = d.getContentPane();
+    JScrollPane sp =
+      new JScrollPane(
+        this,
+        JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+        JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+    c.add(sp);
+    int x = Math.max(0, parent.getWidth()-WIDTH);
+    int y = Math.max(0, parent.getHeight()-HEIGHT);
+    d.setBounds(x,y,WIDTH,HEIGHT);
+    d.setVisible(true);
+  }
+
+  public void hyperlinkUpdate(HyperlinkEvent e) {
+    if (e.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
+      JEditorPane pane = (JEditorPane) e.getSource();
+      if (e instanceof HTMLFrameHyperlinkEvent) {
+        HTMLFrameHyperlinkEvent evt = (HTMLFrameHyperlinkEvent) e;
+        HTMLDocument doc = (HTMLDocument) pane.getDocument();
+        doc.processHTMLFrameHyperlinkEvent(evt);
+      } else {
+        try {
+          pane.setPage(e.getURL());
+        } catch (Throwable t) {
+          t.printStackTrace();
+        }
+      }
+    }
+  }
+
   public static void main(String[] args) {
-    QuickFrame f = new QuickFrame("Help test",new JLabel("Nothing here, look at the dialog."));
+    QuickFrame f =
+      new QuickFrame(
+        "Help test",
+        new JLabel("Nothing here, use the close button below to close the test program."));
     new Help().showDialog(f);
-	}
+  }
 }
