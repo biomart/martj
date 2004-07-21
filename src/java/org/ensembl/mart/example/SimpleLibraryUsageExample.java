487c7e9bb66fe06453e2d65a5704ac6028b15a6c
dlondon <>
2004-07-21 12:44:31 +0000

    modified to make use of MartConfig, and matches the example in the documentation.

--- src/java/org/ensembl/mart/example/SimpleLibraryUsageExample.java
+++ src/java/org/ensembl/mart/example/SimpleLibraryUsageExample.java
@@ -20,6 +20,7 @@ package org.ensembl.mart.example;
 import java.sql.SQLException;
 
 import org.ensembl.mart.lib.BasicFilter;
+import org.ensembl.mart.lib.BooleanFilter;
 import org.ensembl.mart.lib.DetailedDataSource;
 import org.ensembl.mart.lib.Engine;
 import org.ensembl.mart.lib.FieldAttribute;
@@ -29,7 +30,12 @@ import org.ensembl.mart.lib.InvalidQueryException;
 import org.ensembl.mart.lib.LoggingUtils;
 import org.ensembl.mart.lib.Query;
 import org.ensembl.mart.lib.SequenceException;
+import org.ensembl.mart.lib.config.AttributeDescription;
 import org.ensembl.mart.lib.config.ConfigurationException;
+import org.ensembl.mart.lib.config.DSConfigAdaptor;
+import org.ensembl.mart.lib.config.DatabaseDSConfigAdaptor;
+import org.ensembl.mart.lib.config.DatasetConfig;
+import org.ensembl.mart.lib.config.FilterDescription;
 
 /**
  * Demonstrates how to construct a Query and execute it against a database.
@@ -66,29 +72,58 @@ public class SimpleLibraryUsageExample {
 				"mysql",
 				"ensembldb.ensembl.org",
 				"3306",
-				"ensembl_mart_19_2",
-				"jdbc:mysql://ensembldb.ensembl.org:3306/ensembl_mart_19_1",
+				"ensembl_mart_22_1",
+				"jdbc:mysql://ensembldb.ensembl.org:3306/ensembl_mart_22_1",
 				"anonymous",
 				null,
 				10,
 				"com.mysql.jdbc.Driver");
+        
+    DSConfigAdaptor adaptor = new DatabaseDSConfigAdaptor(ds, ds.getUser(), true, false, false);
+    
+    DatasetConfig config = adaptor.getDatasetConfigByDatasetInternalName("hsapiens_gene_ensembl", "default");
+        
 		query.setDataSource(ds);
 
 		// dataset query applies to
-		query.setDataset("hsapiens_ensemblgene");
+		query.setDataset(config.getDataset());
 
 		// prefixes for databases we want to use
-		query.setMainTables(
-			new String[] { "hsapiens_ensemblgene", "hsapiens_ensembltranscript" });
+		query.setMainTables(config.getStarBases());
 
 		// primary keys available for sql table joins 
-		query.setPrimaryKeys(new String[] { "gene_id", "transcript_id" });
+		query.setPrimaryKeys(config.getPrimaryKeys());
 
-		// Attribute to return
-		query.addAttribute(new FieldAttribute("gene_stable_id"));
+		// Attributes to return
+    AttributeDescription adesc = config.getAttributeDescriptionByInternalName("gene_stable_id");
+    
+		query.addAttribute(new FieldAttribute(adesc.getField(), adesc.getTableConstraint(), adesc.getKey()));
 
-		// Filter, only consider chromosome 3
-		query.addFilter(new BasicFilter("chr_name", "=", "3"));
+    adesc = config.getAttributeDescriptionByInternalName("chr_name");
+    query.addAttribute(new FieldAttribute(adesc.getField(), adesc.getTableConstraint(), adesc.getKey()));
+    
+    adesc = config.getAttributeDescriptionByInternalName("mouse_ensembl_id");
+    query.addAttribute(new FieldAttribute(adesc.getField(), adesc.getTableConstraint(), adesc.getKey()));
+        
+    adesc = config.getAttributeDescriptionByInternalName("mouse_dn_ds");
+    query.addAttribute(new FieldAttribute(adesc.getField(), adesc.getTableConstraint(), adesc.getKey()));
+    
+    String name = "chr_name";    
+    FilterDescription fdesc = config.getFilterDescriptionByInternalName(name);
+    
+    //note, the config system actually masks alot of complexity with regard to filters by requiring the internalName
+    //again when calling the getXXX methods
+		query.addFilter(new BasicFilter(fdesc.getField(name), fdesc.getTableConstraint(name), fdesc.getKey(name), "=", "22"));
+
+    name = "mmusculus_homolog";
+    fdesc = config.getFilterDescriptionByInternalName(name);
+    
+    //note there are different types of BooleanFilter
+    if (fdesc.getType(name).equals("boolean"))
+      query.addFilter(new BooleanFilter(fdesc.getField(name), fdesc.getTableConstraint(name), fdesc.getKey(name), BooleanFilter.isNotNULL, null));
+    else
+      query.addFilter(new BooleanFilter(fdesc.getField(name), fdesc.getTableConstraint(name), fdesc.getKey(name), BooleanFilter.isNotNULL_NUM, null));
+    
 
 		//Execute the Query and print the results to stdout.
 		engine.execute(
