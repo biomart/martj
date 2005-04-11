31385afd1551a391ac0eff855bde46dfe96784d2
ds5 <ds5@sanger.ac.uk>
2005-04-11 09:41:15 +0000

    fixed dataset titles

--- src/java/org/ensembl/mart/editor/MartEditor.java
+++ src/java/org/ensembl/mart/editor/MartEditor.java
@@ -638,7 +638,7 @@ public class MartEditor extends JFrame implements ClipboardOwner {
           conn = ds.getConnection();
           dbutils = new DatabaseDatasetConfigUtils(dscutils, ds);
           valid = true;
-          connection = "MartEditor (CONNECTED TO " + databaseDialog.getDatabase() + "/"+databaseDialog.getSchema()+" AS "+databaseDialog.getUser()+")";
+          connection = "MartEditor (CONNECTED TO " + databaseDialog.getDatabase() + "/"+databaseDialog.getSchema()+" AS "+databaseDialog.getUser()+")";		  
         } catch (SQLException e) {
           ds = null;	
           connection = "MartEditor (NO DATABASE CONNECTION)";	
@@ -715,7 +715,7 @@ public class MartEditor extends JFrame implements ClipboardOwner {
       if (intName == null)
         return;
 
-      DatasetConfigTreeWidget frame = new DatasetConfigTreeWidget(null, this, null, user, dataset, intName, null);
+      DatasetConfigTreeWidget frame = new DatasetConfigTreeWidget(null, this, null, user, dataset, intName, databaseDialog.getSchema());
       frame.setVisible(true);
       desktop.add(frame);
       try {
@@ -739,9 +739,9 @@ public class MartEditor extends JFrame implements ClipboardOwner {
       disableCursor();
       DatasetConfig dsConfig = ((DatasetConfigTreeWidget) desktop.getSelectedFrame()).getDatasetConfig();
       
-      if (!dsConfig.getAdaptor().getDataSource().getDatabaseName().equals(database)){
+      if (!dsConfig.getAdaptor().getDataSource().getSchema().equals(databaseDialog.getSchema())){
       	// NM the widget still has its adaptor - could switch connection
-		int choice = JOptionPane.showConfirmDialog(this,"You are exporting this XML to a new database: " + database +"\nChange connection?", "", JOptionPane.YES_NO_OPTION);
+		int choice = JOptionPane.showConfirmDialog(this,"You are exporting this XML to a new schema: " + databaseDialog.getSchema() +"\nChange connection?", "", JOptionPane.YES_NO_OPTION);
       	if (choice == 0){databaseConnection();}
       }
       String dset = dsConfig.getDataset();
