e53ce9d4019b97351179af5b2b84461144e213d1
arek <arek@sanger.ac.uk>
2005-04-14 12:38:29 +0000

    minor fix

--- src/java/org/ensembl/mart/builder/SourceSchema.java
+++ src/java/org/ensembl/mart/builder/SourceSchema.java
@@ -8,100 +8,91 @@ package org.ensembl.mart.builder;
 
 import java.util.*;
 
-
 /**
- * @author <a href="mailto: arek@ebi.ac.uk">Arek Kasprzyk</a>
- *
+ * @author <a href="mailto: arek@ebi.ac.uk">Arek Kasprzyk </a>
  * 
+ *  
  */
 public class SourceSchema {
 
 	ArrayList linked_tables = new ArrayList();
+
 	DBAdaptor adaptor;
+
 	MetaDataResolver resolver;
+
 	String dataset;
-	
-	public SourceSchema (String config){
-		
+
+	public SourceSchema(String config) {
+
 		DBAdaptor adaptor = new DBAdaptor(config);
-		
+
 		MetaDataResolver resolver = null;
-		
-		if (adaptor.rdbms.equals("mysql")){
-		resolver = new MetaDataResolverFKNotSupported(adaptor);
-		} else if (adaptor.rdbms.equals("oracle")){
-		resolver = new MetaDataResolverFKSupported(adaptor);
-		}
-		else if (adaptor.rdbms.equals("postgresql")){
+
+		if (adaptor.rdbms.equals("mysql")) {
+			resolver = new MetaDataResolverFKNotSupported(adaptor);
+		} else if (adaptor.rdbms.equals("oracle")) {
 			resolver = new MetaDataResolverFKSupported(adaptor);
-		}
-		this.adaptor=adaptor;
-		this.resolver=resolver;
-		
+		} else if (adaptor.rdbms.equals("postgresql")) {
+			resolver = new MetaDataResolverFKSupported(adaptor);
+		} 
+		this.adaptor = adaptor;
+		this.resolver = resolver;
+
 	}
-	
-	
-	
-	public LinkedTables createLinkedTables(String main_name,Table [] tables){
-		
+
+	public LinkedTables createLinkedTables(String main_name, Table[] tables) {
+
 		Table main = resolver.getMainTable(main_name);
 		LinkedTables linked = new LinkedTables();
 		linked.setMainTable(main);
 		linked.setReferencedTables(tables);
 		return linked;
-	
+
 	}
-	
-	
-	public Column [] getTableColumns (String name){
-	
+
+	public Column[] getTableColumns(String name) {
+
 		return resolver.getReferencedColumns(name);
 	}
-	
-	
-	
-	public Table [] getReferencedTables (String table_name){
-		
+
+	public Table[] getReferencedTables(String table_name) {
+
 		return resolver.getReferencedTables(table_name);
 	}
-	
-	
-	public Table getMainTable (String table_name){
-		
+
+	public Table getMainTable(String table_name) {
+
 		return resolver.getMainTable(table_name);
-		
+
 	}
-	
-	
-	
+
 	/**
 	 * @return Returns the linkedTables.
 	 */
-	public LinkedTables [] getLinkedTables() {
-		
-		LinkedTables [] b = new LinkedTables[linked_tables.size()];
-		return (LinkedTables []) linked_tables.toArray(b);	
-		
-	}
-	
-	public void addLinkedTables(LinkedTables linked){
-		this.linked_tables.add(linked);
-		
-		
+	public LinkedTables[] getLinkedTables() {
+
+		LinkedTables[] b = new LinkedTables[linked_tables.size()];
+		return (LinkedTables[]) linked_tables.toArray(b);
+
 	}
 
-	public LinkedTables getLinkedTablesByFinalTableName (String name){
-		
-		LinkedTables [] linked = getLinkedTables();
+	public void addLinkedTables(LinkedTables linked) {
+		this.linked_tables.add(linked);
+
+	}
+
+	public LinkedTables getLinkedTablesByFinalTableName(String name) {
+
+		LinkedTables[] linked = getLinkedTables();
 		LinkedTables link = new LinkedTables();
-		
-		for (int i=0;i<linked.length; i++){
-			if (linked[i].final_table_name.equals(name)){
-				link = linked[i];		
+
+		for (int i = 0; i < linked.length; i++) {
+			if (linked[i].final_table_name.equals(name)) {
+				link = linked[i];
 			}
 		}
 		return link;
 	}
-	
-		
+
 }
