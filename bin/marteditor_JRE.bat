f26b8f53256d2e5a0913637a338dd3736851b93e
arek <arek@sanger.ac.uk>
2005-06-12 17:57:19 +0000

    postgres jar included

--- bin/marteditor_JRE.bat
+++ bin/marteditor_JRE.bat
@@ -1,1 +1,1 @@
-..\jre\windows\bin\java -ea -cp ..\build\classes;..\lib\martj.jar;..\lib\log4j-1.2.6.jar;..\lib\ecp1_0beta.jar;..\lib\mysql-connector-java-3.0.16-ga-bin.jar;..\lib\ensj-util.jar;..\lib\jdom.jar;..\lib\ojdbc14.jar;.. org.ensembl.mart.editor.MartEditor
+..\jre\windows\bin\java -ea -cp ..\build\classes;..\lib\martj.jar;..\lib\log4j-1.2.6.jar;..\lib\ecp1_0beta.jar;..\lib\mysql-connector-java-3.0.16-ga-bin.jar;..\lib\pg73jdbc3.jar;..\lib\ensj-util.jar;..\lib\jdom.jar;..\lib\ojdbc14.jar;.. org.ensembl.mart.editor.MartEditor
