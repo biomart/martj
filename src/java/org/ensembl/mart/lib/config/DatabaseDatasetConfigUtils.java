3527cdf2155488ee3a1ade09f172622bbaafcdf1
arek <arek@sanger.ac.uk>
2005-12-03 13:50:42 +0000

    meta_configuration now created with modified datetime for mysql Need to be properly implemented plus added to postres and oracle

--- src/java/org/ensembl/mart/lib/config/DatabaseDatasetConfigUtils.java
+++ src/java/org/ensembl/mart/lib/config/DatabaseDatasetConfigUtils.java
@@ -1610,7 +1610,7 @@ public class DatabaseDatasetConfigUtils {
     String CREATETABLE= "create table " +getSchema()[0]+".meta_configuration";
     String MYSQL_META    = CREATETABLE+"(internalName varchar(100), displayName varchar(100), dataset varchar(100), " +
     		"description varchar(200), xml longblob, compressed_xml longblob, MessageDigest blob, " +
-    		"type varchar(20), visible int(1) unsigned, version varchar(25),datasetID int not null, UNIQUE (dataset),PRIMARY KEY(datasetID))";
+    		"type varchar(20), visible int(1) unsigned, version varchar(25),datasetID int not null, modified DATETIME NOT NULL,UNIQUE (dataset),PRIMARY KEY(datasetID))";
     String MYSQL_USER="CREATE TABLE meta_user ( datasetID int, user varchar(100))";
     String ORACLE_META   = CREATETABLE+" (internalname varchar2(100), displayname varchar2(100), dataset varchar2(100), description varchar2(200), xml clob, compressed_xml blob, messagedigest blob, type varchar2(100), visible number(1), version varchar2(25))";
     String POSTGRES_META = CREATETABLE+"(internalname varchar(100), displayname varchar(100), dataset varchar(100), description varchar(200), xml text, compressed_xml bytea, MessageDigest bytea, type varchar(20), visible integer, version varchar(25))";
@@ -1639,6 +1639,8 @@ public class DatabaseDatasetConfigUtils {
 			  
 			  PreparedStatement ps1=conn.prepareStatement(CREATE_USER);
 			  ps1.executeUpdate();
+			  
+			  System.out.println("created meta tables");
 			  
 			  conn.close();
 			} catch (SQLException e) {
