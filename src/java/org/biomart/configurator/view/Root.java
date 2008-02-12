0b63837acebefafae19369161894399f18248a00
syed <syed@sanger.ac.uk>
2008-02-12 14:34:06 +0000

    adding martConfigurator architecture

---
+++ src/java/org/biomart/configurator/view/Root.java
@@ -0,0 +1,19 @@
+package org.biomart.configurator;
+
+public abstract class Root {
+
+	public Root() {
+		super();
+		// TODO Auto-generated constructor stub
+		System.out.println("Root Constructor: yam");
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Root Main: yam");
+	}
+
+}
---
+++ src/java/org/biomart/configurator/view/configuratorGUI.java
@@ -0,0 +1,13 @@
+package org.biomart.configurator.view.CVS;
+
+public class configuratorGUI {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
